a8fcc5b89adb97920754b5f9a6d9165d55ebc8a0
FemputerTech <mleicht92@gmail.com>
2025-02-16 19:16:17 -0800

    gh-pages doesn't support browser router

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { ButtonWidget, WindowWidget } from './components/widgets';
 import Layout from './components/layout';
 
